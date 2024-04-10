065482febd8d6c355f73c1e5bab32847f0a87a2c
lezernack <luks.ezck@gmail.com>
2024-04-10 16:53:15 -0600

    added code

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 require("dotenv").config();
 
 const express = require("express");
+const app = express;
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(prototype, () => console.log(`server is running on port: ${PORT}`));
