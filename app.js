3c18ab8e2715432ec0d701f7adc0128a88622d23
lezernack <luks.ezck@gmail.com>
2024-04-15 16:34:58 -0600

    changed and added code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const expressLayouts = require("express-ejs-layout");
 const express = require("express");
 const app = express();
 const PORT = 4000;
@@ -7,8 +8,14 @@ const PORT = 4000;
 const connectDB = require("./server/config/db");
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.set("Layout", "./Layouts/main");
+app.set("view enging", "ejs");
+
+app.use("/", require("./server/routes/main"));
 
 app.listen(PORT, () => console.log(`server is running on port: ${PORT}`));
