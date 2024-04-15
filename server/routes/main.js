5a2b0660650d3143c7b11171b668aa60b5380e17
lezernack <luks.ezck@gmail.com>
2024-04-15 16:53:15 -0600

    added new file

---
+++ server/routes/main.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/post');
+
+// Home Page
+router("/", async (req,res)=>{
+    const locals ={
+        title:,
+    }
+})
