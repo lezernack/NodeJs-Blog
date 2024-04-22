33993b747d8158b1b44a291874b014eb5bf0495c
lezernack <luks.ezck@gmail.com>
2024-04-22 13:37:02 -0600

    added admin files/folders

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
+const methodOverride = require("method-override");
+const session = require("express-session");
+
 const connectDB = require("./server/config/db");
 connectDB();
 
@@ -16,6 +21,22 @@ app.use(express.static("public"));
 app.set("Layout", "./Layouts/main");
 app.set("view enging", "ejs");
 
+app.use(cookieParser());
+app.use(methodOverride("_method"));
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
+  })
+);
+
+// Duplicat your route file below to also have an admin instead of main.
 app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
 
 app.listen(PORT, () => console.log(`server is running on port: ${PORT}`));
