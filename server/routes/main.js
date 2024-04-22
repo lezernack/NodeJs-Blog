3b6ce0fb8fe7042f10a3867939d7ec3e9c648f9a
lezernack <luks.ezck@gmail.com>
2024-04-22 13:05:35 -0600

    finished all of the code for this part

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,14 +4,34 @@ const Post = require("../models/post");
 
 // Home Page
 router.get("/", async (req, res) => {
-  const locals = {
-    title: "NodeJS Blog",
-    description: "A Blog Template application that will be used for your use.",
-  };
-
   try {
-    const data = await Post.find().sort({ title: "desc" });
-    res.render("index", { locals, data });
+    const locals = {
+      title: "NodeJS Blog",
+      description: "Simple Blog created with NodeJs, Express & MonogDb",
+    };
+
+    let perPage = 3;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { title: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    // Count is Deprecated - please use countDocuments({}) instead
+    // const count = await Post.count();
+    const count = await Post.countDocuments({});
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = next <= Math.ceil.apply(count / perPage);
+    const hasNextPagePlus = nextPage <= Math.ceil(count * perPage);
+
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+      prevPage: hasNextPagePlus ? page - 1 : null,
+    });
   } catch (error) {
     console.log(error);
   }
@@ -29,7 +49,10 @@ router.get("/post/:id", async (req, res) => {
       description:
         "A Blog Template application that will be used for your use.",
     };
-    res.render("post", { locals, data });
+    res.render("post", {
+      locals,
+      data,
+    });
   } catch (error) {
     console.log(error);
   }
